bd2630e6a9dce484e573fe4db9a2281d66bff4e8
Vkpn <98121055+Vkpn@users.noreply.github.com>
2022-08-25 16:10:56 -0300

    crianção da base do projeto

--- Projeto Loja Virtual/projeto_loja_virtual/main/models.py
+++ Projeto Loja Virtual/projeto_loja_virtual/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    imagem = models.ImageField(upload_to='produtos/', blank=True)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('nome',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.nome
